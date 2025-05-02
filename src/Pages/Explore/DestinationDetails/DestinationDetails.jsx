3ee537727ae1151888c137643f8772b54c617e2e
Marouane Dhambri <dhambrimarouane@gmail.com>
2025-05-02 09:34:49 +0100

    finished preparing edit destination form

--- src/Pages/Explore/DestinationDetails/DestinationDetails.jsx
+++ src/Pages/Explore/DestinationDetails/DestinationDetails.jsx
@@ -1,13 +1,13 @@
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { motion } from 'framer-motion';
 import { API, API_HOST } from '../../../configs/api';
 import { hideFullscreen, showFullscreen, variants } from '../../../configs/helper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClose, faUpload } from '@fortawesome/free-solid-svg-icons';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faClose, faUpload, faSpinner, faEdit, faPlus } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
 
 import { getOneDestination } from '../../../redux/slices/destinations/destinationsAsyncThunk';
 import { deleteOneImage, uploadImage } from '../../../redux/slices/images/imagesAsyncThunk';
@@ -16,12 +16,26 @@ import './DestinationDetails.css';
 
 const DestinationDetails = () => {
     const dispatch = useDispatch()
-    const { OneDestination, error, isLoading, status } = useSelector((state) => state.destinations.OneDestination)
+    const params = useParams()
     const images = useSelector((state) => state.images.deleteOneImage)
     const OneImageUpload = useSelector((state) => state.images.OneImageUpload)
-    const params = useParams()
+    const OneDestination = useSelector((state) => state.destinations.OneDestination.OneDestination)
+
+    const [isEdit, setIsEdit] = useState(false)
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [todo, setTodo] = useState([])
+    const [bestTime, setBestTime] = useState('')
+    const [featured, setFeatured] = useState(false)
+    const [isLive, setIsLive] = useState(false)
 
     const destinationId = params.id
+    const token = localStorage.getItem('token')
+
+    const toggleEdit = () => {
+        setIsEdit(!isEdit)
+    }
 
     const handleImageUpload = (e) => {
         if (e.target.files.length > 0) {
@@ -29,7 +43,7 @@ const DestinationDetails = () => {
             formData.append('file', e.target.files[0])
             formData.append('imageType', "destination")
             formData.append('assiciationId', params.id)
-            formData.append('isMain', false)
+            formData.append('isMain', 'false')
             dispatch(uploadImage(formData))
         }
     }
@@ -40,7 +54,7 @@ const DestinationDetails = () => {
             formData.append('file', e.target.files[0])
             formData.append('imageType', "destination")
             formData.append('assiciationId', params.id)
-            formData.append('isMain', true)
+            formData.append('isMain', 'true')
             dispatch(uploadImage(formData))
         }
     }
@@ -49,11 +63,34 @@ const DestinationDetails = () => {
         dispatch(deleteOneImage(imageId))
     }
 
+    const handleToDoChange = (e, index) => {
+        const newTodo = [...todo]
+        newTodo[index] = e.target.value
+        setTodo(newTodo)
+    }
+
+    const handleDeleteToDo = (index) => {
+        const newTodo = [...todo]
+        newTodo.splice(index, 1)
+        setTodo(newTodo)
+    }
+
     useEffect(() => {
         if (destinationId) {
             dispatch(getOneDestination(destinationId))
         }
     }, [destinationId, images.status, OneImageUpload.status])
+
+    useEffect(() => {
+        if (OneDestination) {
+            setDescription(OneDestination.description)
+            setTitle(OneDestination.title)
+            setTodo(OneDestination.toDo)
+            setBestTime(OneDestination.bestTime)
+            setFeatured(OneDestination.featured)
+            setIsLive(OneDestination.isLive)
+        }
+    }, [OneDestination])
 
     return (
         <motion.div
@@ -70,52 +107,121 @@ const DestinationDetails = () => {
                         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${API_HOST + API.images.upload + OneDestination?.mainPicture})`,
                     }}
                 >
-                    <h2>{OneDestination?.title}</h2>
+                    {
+                        !isEdit ?
+                            <h2>{OneDestination?.title}</h2> :
+                            <div>
+                                <label>Title</label>
+                                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                            </div>
+                    }
                 </div>
                 <div className="destination-content">
                     {
-                        <label htmlFor="file-upload" className="custom-file-upload">
+                        !isEdit && token && <label style={{ left: '18px' }} htmlFor="main-file-upload" className="custom-file-upload">
                             {
-                                isLoading ?
+                                OneImageUpload.isLoading ?
                                     <FontAwesomeIcon icon={faSpinner} spin />
                                     :
                                     <FontAwesomeIcon icon={faUpload} />
                             }
                         </label>
                     }
-                    <input id="file-upload" type="file" value="" onChange={handleMainImageUpload} />
-                    <p>
-                        {OneDestination?.description}
-                    </p>
+                    {
+                        !isEdit && token && <FontAwesomeIcon onClick={toggleEdit} className="edit-button" icon={faEdit} />
+                    }
+                    <input id="main-file-upload" type="file" value="" onChange={handleMainImageUpload} />
+                    {
+                        isEdit && <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <div onChange={(e) => setFeatured(e.target.checked)} className="form-check form-switch">
+                                <input className="form-check-input" checked={featured} type="checkbox" role="switch" id="featured" />
+                                <label className="form-check-label" htmlFor="featured">Featured</label>
+                            </div>
+                            <div onChange={(e) => setIsLive(e.target.checked)} className="form-check form-switch">
+                                <input className="form-check-input" checked={isLive} type="checkbox" role="switch" id="isLive" />
+                                <label className="form-check-label" htmlFor="isLive">Live</label>
+                            </div>
+                        </div>
+                    }
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        {
+                            !isEdit ?
+                                <p style={{ margin: '0' }}>
+                                    {OneDestination?.description}
+                                </p> :
+                                <div style={{ width: '100%' }}>
+                                    <label>Description</label>
+                                    <textarea onChange={(e) => setDescription(e.target.value)} value={description} />
+                                </div>
+                        }
+                    </div>
                     <h3>Things to Do:</h3>
-                    <ul>
-                        {OneDestination?.toDo.map((item, index) => (
-                            <li key={index}>{item}</li>
-                        ))}
-                    </ul>
+                    {
+                        !isEdit ? <ul>
+                            {OneDestination?.toDo.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul> :
+                            <div>
+                                {todo?.map((item, index) => (
+                                    <div key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '10px' }}>
+                                        <input type="text" style={{ margin: '0 10px 0 0' }} value={item} onChange={(e) => handleToDoChange(e, index)} />
+                                        <Button style={{ height: '49px' }} variant="danger" onClick={() => handleDeleteToDo(index)}>
+                                            Delete
+                                        </Button>
+                                    </div>
+                                ))}
+                                <Button style={{ color: 'black', backgroundColor: '#C7D9DD', border: 'none', height: 48 }} onClick={() => {
+                                    setTodo([...todo, ''])
+                                }}>
+                                    <FontAwesomeIcon style={{ marginRight: '10px' }} icon={faPlus} />
+                                    Add
+                                </Button>
+                            </div>
+                    }
                     <h3>Best Time to Visit:</h3>
-                    <p>
-                        {OneDestination?.bestTime}
-                    </p>
-                    <div className="gallery-grid">
-                        <label htmlFor="file-upload" className="gallery-item" style={{
-                            background: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4))`, display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer'
-                        }}>
-                            {
-                                isLoading ?
-                                    <FontAwesomeIcon icon={faSpinner} spin />
-                                    :
-                                    <FontAwesomeIcon icon={faUpload} />
-                            }
-                        </label>
+                    {
+                        !isEdit ?
+                            <p style={{ margin: '0' }}>
+                                {OneDestination?.bestTime}
+                            </p> :
+                            <div style={{ width: '100%' }}>
+                                <label>Best Time</label>
+                                <input type="text" value={bestTime} onChange={(e) => setBestTime(e.target.value)} />
+                            </div>
+                    }
+                    <hr />
+                    {
+                        isEdit && <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Button style={{ color: 'black', backgroundColor: '#C7D9DD', border: 'none', height: 48 }} onClick={toggleEdit}>
+                                Save changes
+                            </Button>
+                            <Button style={{ height: 48 }} variant="secondary" onClick={toggleEdit}>
+                                Close
+                            </Button>
+                        </div>
+                    }
+                    {!isEdit && <div className="gallery-grid">
+                        {
+                            token && <label htmlFor="file-upload" className="gallery-item" style={{
+                                background: `#C7D9DD`, display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', minHeight: "150px", borderRadius: '10px'
+                            }}>
+                                {
+                                    OneImageUpload.isLoading ?
+                                        <FontAwesomeIcon style={{ width: '50px', height: '50px' }} icon={faSpinner} spin />
+                                        :
+                                        <FontAwesomeIcon style={{ width: '50px', height: '50px' }} icon={faUpload} />
+                                }
+                            </label>
+                        }
                         <input id="file-upload" type="file" value="" onChange={handleImageUpload} />
                         {
                             OneDestination?.images.map((image, index) => {
                                 return (
                                     <div key={index} className="gallery-item">
                                         {
-                                            images.isLoading ? <FontAwesomeIcon key={index} className='loading_delete_icon' icon={faSpinner} spin /> :
-                                                <FontAwesomeIcon key={index} onClick={() => handledeleteImage(image)} className='delete_icon' icon={faClose} />
+                                            images.isLoading ? token && <FontAwesomeIcon key={index} className='loading_delete_icon' icon={faSpinner} spin /> :
+                                                token && <FontAwesomeIcon key={index} onClick={() => handledeleteImage(image)} className='delete_icon' icon={faClose} />
                                         }
                                         <img
                                             src={API_HOST + API.images.upload + image}
@@ -126,7 +232,7 @@ const DestinationDetails = () => {
                                 )
                             })
                         }
-                    </div>
+                    </div>}
                 </div>
             </section>
             <div id="fullscreen-overlay" onClick={hideFullscreen}>
